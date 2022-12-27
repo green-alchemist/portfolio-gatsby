da7e676a7d736916ffc07909ffc9d6eadabfac1f
sigma-us <kyleconley2@gmail.com>
2022-12-26 18:42:11 -0800

    new branch, dirty commit, working so far though

---
+++ content/hello-two/index.md
@@ -0,0 +1,7 @@
+---
+title: Hello World Two
+date: "2022-05-01T22:12:03.284Z"
+description: "Hello World Two"
+---
+
+## Second POST Hello World I Love you
---
+++ gatsby-node.ts
@@ -0,0 +1,81 @@
+import type { GatsbyNode } from "gatsby"
+import { createFilePath } from "gatsby-source-filesystem"
+
+type Person = {
+  id: number
+  name: string
+  age: number
+}
+
+export const createPages: GatsbyNode["createPages"] = async ({ graphql, actions, reporter }) => {
+  const seen = new Set()
+
+  const { data }: any = await graphql(`
+    query {
+      allMarkdownRemark {
+        nodes {
+          id
+          frontmatter {
+            date
+            description
+            title
+          }
+          html
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  reporter.info("WHATS HAPPENING")
+
+  // reporter.info(data)
+
+}
+
+
+// Example of adding custom fields to a node
+export const onCreateNode: GatsbyNode["onCreateNode"] = async ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  console.log("INSIDE : Gatsby-NODE FILE")
+  
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
+
+
+// export const sourceNodes: GatsbyNode["sourceNodes"] = async ({
+//   actions,
+//   createNodeId,
+//   createContentDigest,
+// }) => {
+//   const { createNode } = actions
+
+//   // const data = await getSomeData()
+
+//   // data.forEach((person: Person) => {
+//   //   const node = {
+//   //     ...person,
+//   //     parent: null,
+//   //     children: [],
+//   //     id: createNodeId(`person__${person.id}`),
+//   //     internal: {
+//   //       type: "Person",
+//   //       content: JSON.stringify(person),
+//   //       contentDigest: createContentDigest(person),
+//   //     },
+//   //   }
+
+//   //   createNode(node)
+//   // })
+// }
