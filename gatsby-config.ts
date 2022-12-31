24e052d8ec6370860800f9e339caf121e325d90a
sigma-us <kyleconley2@gmail.com>
2022-12-31 11:34:37 -0800

    cleaner config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -16,35 +16,12 @@ const config: GatsbyConfig = {
     title: `portfolio-2`,
     siteUrl: `https://blog.kconley.com`
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-strapi`,
       options: strapiConfig,
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          // {
-          //   resolve: `gatsby-remark-images`,
-          //   options: {
-          //     maxWidth: 630,
-          //   },
-          // },
-          // {
-          //   resolve: `gatsby-remark-responsive-iframe`,
-          //   options: {
-          //     wrapperStyle: `margin-bottom: 1.0725rem`,
-          //   },
-          // },
-          // `gatsby-remark-prismjs`,
-        ],
-      },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
