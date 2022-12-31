0031e611a10c831ee078a500dc4fcc857cabbb96
Kyle Conley <kyleconley2@gmail.com>
2022-12-31 12:16:17 -0800

    Source-strapi builds pages (#2)
    * rm source-filesystem
    
    * cleaner config
    
    * creates very crude page based on strapi post

--- content/hey/index.md
+++
@@ -1,7 +0,0 @@
----
-title: Hey World
-date: "2022-05-01T22:12:03.284Z"
-description: "Hey world post 2"
----
-
-hey world
--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,21 +1,10 @@
 import type { GatsbyNode } from "gatsby"
-import { createFilePath } from "gatsby-source-filesystem"
-import assert from "node:assert"
 import path from "node:path"
-// const path = require(`path`)
+import assert from "node:assert"
+// import { createFilePath } from "gatsby-source-filesystem"
+
 const blogPost = path.resolve(`./src/templates/blog-post.tsx`)
 
-// type Person1 = {
-//   id: number
-//   name: string
-//   age: number
-// }
-// interface Person {
-//   id: number
-//   name: string
-//   age: number
-//   family: Person1
-// }
 
 export const createPages: GatsbyNode["createPages"] = async ({ graphql, actions }) => {
   // const seen = new Set()
@@ -23,26 +12,41 @@ export const createPages: GatsbyNode["createPages"] = async ({ graphql, actions 
 
   const { data } = await graphql(`
     query BlogPostQuery{
-      allMarkdownRemark {
+      allStrapiPost {
         nodes {
           id
-          frontmatter {
-            date
-            description
-            title
+          body {
+            data {
+              body
+              id
+              internal {
+                description
+                contentFilePath
+                content
+                contentDigest
+                fieldOwners
+                type
+                owner
+                mediaType
+                ignoreType
+              }
+            }
           }
-          html
-          fields {
-            slug
+          slug
+          title
+          publishedAt
+          updatedAt
+          categories {
+            id
           }
         }
       }
     }
   `)
 
-  if (data.errors) throw data.errors
+  // if (data.errors) throw data.errors
 
-  const posts = data.allMarkdownRemark.nodes
+  const posts = data.allStrapiPost.nodes
   if (posts.length > 0) {
     posts.forEach((post: any, index: number) => {
       // const path = post?.fields?.slug
@@ -51,20 +55,17 @@ export const createPages: GatsbyNode["createPages"] = async ({ graphql, actions 
       //   assert.fail(`"${path}" already exists`)
       // }
       // seen.add(path)
-      console.log("POST #: ", index)
-      // console.log("POST Fields: ", data)
-      // console.log("POST internal: ", data)
-
+      console.log("POST #: ", index, post)
 
       const previousPostId = index === 0 ? null : posts[index - 1].id
       const nextPostId = index === posts.length - 1 ? null : posts[index + 1].id
 
       // context gets passed to the page function component for use in the pageQuery
       // component seems to be looking for a file path and not the actual function component which is exported
-      if (post && post.fields && post.fields.slug !== null) {
+      if (post && post.slug !== null) {
 
         createPage({
-          path: post.fields.slug,
+          path: post.slug,
           component: blogPost,
           context: {
             id: post.id,
@@ -83,21 +84,32 @@ export const createPages: GatsbyNode["createPages"] = async ({ graphql, actions 
 export const onCreateNode: GatsbyNode["onCreateNode"] = async ({ node, actions, getNode }) => {
   const { createNodeField } = actions
 
-  // console.log("INSIDE : Gatsby-Node internal type: ")
-  
+  console.log("INSIDE : Gatsby-Node internal type: ", node.internal.type)
+  if (node.internal.type !== 'SitePlugin') {
+    // console.log("Node OBJ: ", node)
+
+  }
   if (node.internal.type === `MarkdownRemark`) {
     // const value = createFilePath({ node, getNode })
     // console.log("VALUE: ", value)
 
-    createNodeField({
-      name: `slug`,
-      node,
-      "value":"value",
-    })
+    // createNodeField({
+    //   name: `slug`,
+    //   node,
+    //   "value":"value",
+    // })
   }
 }
 
 
+
+
+/*  
+  Gatsby-node api reference 
+  https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
+*/
+
+// Example for sourceNodes
 // export const sourceNodes: GatsbyNode["sourceNodes"] = async ({
 //   actions,
 //   createNodeId,
@@ -105,21 +117,21 @@ export const onCreateNode: GatsbyNode["onCreateNode"] = async ({ node, actions, 
 // }) => {
 //   const { createNode } = actions
 
-//   // const data = await getSomeData()
+//   const data = await getSomeData()
 
-//   // data.forEach((person: Person) => {
-//   //   const node = {
-//   //     ...person,
-//   //     parent: null,
-//   //     children: [],
-//   //     id: createNodeId(`person__${person.id}`),
-//   //     internal: {
-//   //       type: "Person",
-//   //       content: JSON.stringify(person),
-//   //       contentDigest: createContentDigest(person),
-//   //     },
-//   //   }
+//   data.forEach((person: Person) => {
+//     const node = {
+//       ...person,
+//       parent: null,
+//       children: [],
+//       id: createNodeId(`person__${person.id}`),
+//       internal: {
+//         type: "Person",
+//         content: JSON.stringify(person),
+//         contentDigest: createContentDigest(person),
+//       },
+//     }
 
-//   //   createNode(node)
-//   // })
+//     createNode(node)
+//   })
 // }
